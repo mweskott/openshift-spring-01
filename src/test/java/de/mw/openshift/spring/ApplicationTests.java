3d698bd367cd45bfff4a3b40586dce6bd24552ad
Markus Weskott <Martt@brain-force.intern>
2020-05-02 11:47:09 +0200

    Spring Initializr

---
+++ src/test/java/de/mw/openshift/spring/ApplicationTests.java
@@ -0,0 +1,13 @@
+package de.mw.openshift.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
